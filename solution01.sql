9cce071d6f802a789010888cabfb9b7570619045
Honoka <kamiya.ai0531@gmail.com>
2026-05-23 19:36:05 -0700

    Update solution01.sql

--- solution01.sql
+++ solution01.sql
@@ -1,20 +1,23 @@
 --Problem: Active User Retention
 --URL: https://datalemur.com/questions/user-retention
 
-WITH june AS (
-  SELECT user_id, EXTRACT(MONTH FROM event_date) AS month
-  FROM user_actions
-  WHERE TO_CHAR(event_date, 'MM/YYYY') = '06/2022'
-  ),
-july AS (
-  SELECT user_id, EXTRACT(MONTH FROM event_date) AS month
-  FROM user_actions
-  WHERE TO_CHAR(event_date, 'MM/YYYY') = '07/2022'
-  ),
-combine AS (
-  SELECT DISTINCT july.user_id, july.month
-  FROM june JOIN july ON june.user_id = july.user_id)
-
-SELECT month, COUNT(*) as monthly_active_users
-FROM combine
-GROUP BY month
+WITH june
+     AS (SELECT user_id,
+                Extract(month FROM event_date) AS month
+         FROM   user_actions
+         WHERE  To_char(event_date, 'MM/YYYY') = '06/2022'),
+     july
+     AS (SELECT user_id,
+                Extract(month FROM event_date) AS month
+         FROM   user_actions
+         WHERE  To_char(event_date, 'MM/YYYY') = '07/2022'),
+     combine
+     AS (SELECT DISTINCT july.user_id,
+                         july.month
+         FROM   june
+                JOIN july
+                  ON june.user_id = july.user_id)
+SELECT month,
+       Count(*) AS monthly_active_users
+FROM   combine
+GROUP  BY month 
