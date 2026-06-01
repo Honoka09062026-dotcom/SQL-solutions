68fe437b829732421f6efdeebeb6d54d76498d97
Honoka <kamiya.ai0531@gmail.com>
2026-06-01 15:38:14 -0700

    Create sulution3.sql

---
+++ sulution3.sql
@@ -0,0 +1,16 @@
+WITH turbotax_years
+     AS (SELECT user_id,
+                Extract(year FROM filing_date) AS filing_year
+         FROM   filed_taxes
+         WHERE  product LIKE '%TurboTax%'),
+     consecutive_check
+     AS (SELECT user_id,
+                filing_year,
+                Lag(filing_year, 2)
+                  OVER (
+                    partition BY user_id
+                    ORDER BY filing_year) AS year_2steps_back
+         FROM   turbotax_years)
+SELECT DISTINCT user_id
+FROM   consecutive_check
+WHERE  filing_year - year_2steps_back = 2 
